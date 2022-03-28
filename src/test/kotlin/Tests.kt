bee817cc4ea7ffd435bbc5b6ddd4c6cc8fee6e23
Ari Aaltonen <ari.aaltonen@op.fi>
2022-03-28 22:16:05 +0300

    Create few dice tests.

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class Tests {
@@ -8,5 +9,30 @@ class Tests {
  	val result = Dice().throws()
         assertEquals(1,result)
 	}
+
+    @Test
+    fun `When dice thrown 100 times then all numbers 1,2,3,4,5 or 6 appear`() {
+        var collectedResults:MutableList<Int> = mutableListOf()
+        for (i in 1..100) {
+            val result = Dice().throws()
+            collectedResults.add(result)
+        }
+        assertTrue(collectedResults.contains(1))
+        assertTrue(collectedResults.contains(2))
+        assertTrue(collectedResults.contains(3))
+        assertTrue(collectedResults.contains(4))
+        assertTrue(collectedResults.contains(5))
+        assertTrue(collectedResults.contains(6))
+    }
+
+    @Test
+    fun `When dice thrown 100 times then value is either 1,2,3,4,5 or 6`() {
+        for (i in 1..100) {
+            val result = Dice().throws()
+            val valueGreaterThatZero = result > 0
+            val valueLessThanSeven = result < 7
+            assertTrue(valueGreaterThatZero && valueLessThanSeven, "Got: $result")
+        }
+    }
 
 }
